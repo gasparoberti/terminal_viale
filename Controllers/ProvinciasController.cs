b1db4363959ecca42a9a24ad1b487c81457aa186
Gaspar <obertigaspar@gmail.com>
2020-06-06 17:44:08 -0300

    se creo el controlador y las vistas para provincia

---
+++ Controllers/ProvinciasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TerminalDeViale.Models;
+
+namespace TerminalDeViale.Controllers
+{
+    [Authorize]
+    public class ProvinciasController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Provincias
+        public ActionResult Index()
+        {
+            return View(db.Provincia.ToList());
+        }
+
+        // GET: Provincias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provincia provincia = db.Provincia.Find(id);
+            if (provincia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provincia);
+        }
+
+        // GET: Provincias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Provincias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,estado,fecha_alta,fecha_baja,usuario_baja,usuario_alta")] Provincia provincia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Provincia.Add(provincia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(provincia);
+        }
+
+        // GET: Provincias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provincia provincia = db.Provincia.Find(id);
+            if (provincia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provincia);
+        }
+
+        // POST: Provincias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,estado,fecha_alta,fecha_baja,usuario_baja,usuario_alta")] Provincia provincia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(provincia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(provincia);
+        }
+
+        // GET: Provincias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provincia provincia = db.Provincia.Find(id);
+            if (provincia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provincia);
+        }
+
+        // POST: Provincias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Provincia provincia = db.Provincia.Find(id);
+            db.Provincia.Remove(provincia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
